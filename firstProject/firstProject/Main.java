9c6f2833b8f4c158f87fab1dc78aec02f783e46e
vveloso <vveloso@192.168.2.33>
2021-08-11 21:36:58 -0300

    Adicionando um comentário.

--- firstProject/firstProject/Main.java
+++ firstProject/firstProject/Main.java
@@ -20,11 +20,19 @@ public class Main {
 		System.out.println("Qual o seu nome?"); // usando println
 		
 		//Ler uma string do console usando a biblioteca Scanner.
-		Scanner in = new Scanner(System.in);
-		String nome = in.nextLine();
+		Scanner name = new Scanner(System.in);
+		String nome = name.nextLine();
 		
 		//Exibir uma string formatada
-		System.out.printf("Olá %s, seja bem vindo!", nome);	
+		System.out.printf("Olá %s, seja bem vindo!", nome);
+		
+		System.out.println("Qual a sua idade?");
+		
+		Scanner old = new Scanner(System.in);
+		String idade = old.nextLine();
+		
+		System.out.printf("Meu Deus %s anos? Que velho", idade);
+		
 
 	}
 
